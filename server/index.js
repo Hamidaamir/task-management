dd54e640a30f015f5b6b02244bc7a5b0a6fc7aeb
Hamidaamir <hamidamir021@gmail.com>
2024-12-09 02:18:56 +0500

    added projects and client with CRUD operations

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
 
 app.use(
   "/graphql",
